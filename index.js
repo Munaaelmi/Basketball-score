3061ec88ea1cf6d92d6d366f4c163dfd94d6c49d
Munaaelmi <munaaelmi@outlook.com>
2023-09-23 19:11:58 +0100

    Developed a basketball score count for home & away

---
+++ index.js
@@ -0,0 +1,35 @@
+let homeCount = 0
+let guestCount = 0
+
+let homeScore = document.getElementById("homescore")
+let guestScore = document.getElementById("guestscore")
+
+function homeAddOne() {
+    homeCount += 1
+    homeScore.textContent = homeCount
+}
+
+function homeAddTwo() {
+    homeCount += 2
+    homeScore.textContent = homeCount
+}
+
+function homeAddThree() {
+    homeCount += 3
+    homeScore.textContent = homeCount
+}
+
+function guestAddOne() {
+    guestCount += 1
+    guestScore.textContent = guestCount
+}
+
+function guestAddTwo() {
+    guestCount += 2
+    guestScore.textContent = guestCount
+}
+
+function guestAddThree() {
+    guestCount += 3
+    guestScore.textContent = guestCount
+}
